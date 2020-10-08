223b92b7184acdd38d0123d7606ca3963d9404a1
Kelly Mears <developers@tinypixel.dev>
2020-10-08 03:48:53 -0700

    fix embed

--- public/embed.js
+++ public/embed.js
@@ -1,17 +1,22 @@
-window.landbackEmbedFrame = function({}) {
-  var writeIframe = function(partnerId) {
+window.landbackEmbedFrame = function() {
+  var writeIframe = function() {
     return document.write(
       '<iframe id="embed-landback-map" src="https://landback-map.netlify.app" width="100%" scrolling="no" height="400" frameborder="0"></iframe>',
     )
   }
 
-  writeIframe(partnerId)
+  writeIframe()
 
   var script = window.document.createElement('script')
+
   script.src =
     'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.2.11/iframeResizer.min.js'
+
   script.onload = function() {
-    window.iFrameResize({ log: false, checkOrigin: false },
+    window.iFrameResize({
+        log: false,
+        checkOrigin: false,
+      },
       '#embed-landback-map',
     )
   }
